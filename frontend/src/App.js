9792857b10c260f26d0f6e04091fe8e537199506
Subinoy <subinoy2001@gmail.com>
2022-01-03 22:37:24 +0530

    popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl';
-import {Room} from "@material-ui/icons"
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import {Room, Star} from "@material-ui/icons"
+import "./app.css"
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -23,6 +24,30 @@ function App() {
       >
         <Room style={{fontSize: viewport.zoom * 7, color: "slateblue" }}/>
       </Marker>
+      {/* <Popup
+          latitude={48.858222}
+          longitude={2.294500}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left" >
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">Eiffel Tower</h4>
+            <label>Review</label>
+            <p className='desc'>Beautiful Place. I like it.</p>
+            <label>Rating</label>
+            <div className="stars">
+            <Star className="star"/>
+            <Star className="star"/>
+            <Star className="star"/>
+            <Star className="star"/>
+            <Star className="star"/>
+            </div>
+            <label>Information</label>
+            <span className="username">Created by <b>safak</b></span>
+            <span className="date">1 hour ago</span>
+          </div>
+        </Popup> */}
     </ReactMapGL>
 
     </div>
